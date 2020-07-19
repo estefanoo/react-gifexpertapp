dbb14496065a765f9821521c8a7fd00e5cd69a59
estefanoo <estefanocarlos54@gmail.com>
2020-07-19 16:56:24 -0300

    primer commit

---
+++ docs/precache-manifest.0094d3f453ff757a1eac88fa5cbaa999.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376f5dff576a8dbe04b40792f1374e04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ddf1b90aa503b7c0877",
+    "url": "/static/css/main.08fc585b.chunk.css"
+  },
+  {
+    "revision": "f4f94b6f63ef6d271e4b",
+    "url": "/static/js/2.527f945a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.527f945a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddf1b90aa503b7c0877",
+    "url": "/static/js/main.4556bf6d.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
